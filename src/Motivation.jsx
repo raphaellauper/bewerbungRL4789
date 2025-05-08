2515542be6fbb0732736a32fe153eec024c299a4
raphaellauper <laurel.paraphe@gmail.com>
2025-05-09 00:05:38 +0200

    Update Motivation.jsx

--- src/Motivation.jsx
+++ src/Motivation.jsx
@@ -2,7 +2,7 @@ export default function Motivation() {
   return (
     <section>
       <h1 className="text-2xl font-bold mb-4">Motivationsschreiben</h1>
-      <p>Diese Bewerbung ist mehr als ein PDF – sie ist ein System. Modular, skalierbar, lokal, erweiterbar. Ich zeige damit nicht nur, was ich kann – sondern wie ich denke.</p>
+      <p>Diese Bewerbung ist mehr als ein PDF. Sie ist ein System. Modular, skalierbar, lokal, erweiterbar. Ich zeige damit nicht nur, was ich kann – sondern wie ich denke.</p>
     </section>
   );
 }
