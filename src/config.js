83d7e8d56284e31467494ec6c9cd3134370f138f
raphaellauper <laurel.paraphe@gmail.com>
2025-05-09 05:06:02 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+  includeGlossar: true,
+  includeDiplome: true,
+  pdfName: 'bewerbung_4789.pdf',
+  mail: 'laurelparaphe@gmail.com',
+  adresse: 'Bahnhofstrasse 56, 3127 Mühlethurnen',
+  jahr: new Date().getFullYear()
+};
