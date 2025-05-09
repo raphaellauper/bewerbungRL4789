2652b68cef0709d2b2f78ada78f9210745dd0dc6
raphaellauper <laurel.paraphe@gmail.com>
2025-05-09 05:53:56 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export default {
   includeDiplome: true,
   pdfName: 'Raphael Lauper',
   mail: 'laurel.paraphe@gmail.com',
-  tel: '+41 79 235 68 89'
+  tel: '+41 79 235 68 89',
   adresse: 'Bahnhofstrasse 56, 3127 Mühlethurnen',
   jahr: new Date().getFullYear()
 };
