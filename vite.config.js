6923e8338ec182b3811884cbfd63707dc8873d4f
raphaellauper <laurel.paraphe@gmail.com>
2025-05-09 05:21:45 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['/html2pdf.custom.js']
+    }
+  }
+});
