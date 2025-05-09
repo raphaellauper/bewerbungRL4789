ec31f228c42d6b30c0ff6cd72068539f99a3983c
raphaellauper <laurel.paraphe@gmail.com>
2025-05-09 02:33:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
   "stelle": "Produktdesigner",
   "ansprache": "Sehr geehrte Frau Pfeiffer",
   "pdfName": "bewerbung_duscholux.pdf",
-  "includeDiplome": false,
+  "includeDiplome": true,
   "farbe": "#1f2937",
   "accent": "#16a34a"
 }
